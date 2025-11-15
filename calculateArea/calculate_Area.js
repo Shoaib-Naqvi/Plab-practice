bc2dd79da698f9448446fbcffbe72d49f15653df
Shoaib-Naqvi34 <shoaibnaqviit1434@gmail.com>
2025-11-14 23:40:16 -0500

    message

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -9,3 +9,20 @@ function calculateArea() {
 
  document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
 }
+
+function calculateTotalAmount(amounts) {
+        const totalAmount = amounts.reduce((acc, val) => acc + val, 0);
+        return totalAmount;
+      }
+      
+      document.getElementById('calculateBtn').addEventListener('click', () => {
+        const amounts = [
+          parseFloat(document.getElementById('grocery1').value) || 0,
+          parseFloat(document.getElementById('grocery2').value) || 0,
+          parseFloat(document.getElementById('grocery3').value) || 0,
+        ];
+      
+        const total = calculateTotalAmount(amounts);
+        document.getElementById('totalAmount').textContent = `Total amount spent on groceries: $${total.toFixed(2)}`;
+      });
+
